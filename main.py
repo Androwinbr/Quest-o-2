dc731bac6d20e3f36ffee80f8822eb0cbbe6abd2
Androwinbr <vinicius.cgobbi2004@gmail.com>
2020-10-16 12:57:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+#crie três variáveis, multiplique o valor da primeira por 2, some esse resultado com as duas outras variáveis, e imprima o resultado desta última operação.
+x = 32
+y = 65
+z = 45
+soma = x * 2 + y + z
+print("valor final é" , soma)
